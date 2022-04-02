8f677032a3265e7a35c178eb8c8dc316c69da145
leo-step <leo.stepanewk@gmail.com>
2022-04-02 11:56:22 -0400

    react

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
+from flask_cors import CORS
 from coursegraph import CourseGraph
+from secrets import *
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
+app.secret_key = APP_SECRET_KEY
+CORS(app) #comment this on deployment
+'''
 course_graph = CourseGraph("adjlist.txt")
 
 code_to_id = {}
@@ -12,10 +16,12 @@ for line in labels.readlines():
     fields = line.split(',')
     id_to_code[int(fields[0])] = fields[1]
     code_to_id[fields[1]] = int(fields[0])
+'''
 
 @app.route("/")
 def index():
-    return str(list(map(lambda x: id_to_code[x], course_graph.get_prereqs(code_to_id["COS 324"]))))
+    return app.send_static_file("index.html")
+    #return str(list(map(lambda x: id_to_code[x], course_graph.get_prereqs(code_to_id["COS 324"]))))
 
 if __name__ == "__main__":
     app.run()
